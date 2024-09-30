e5ea554cae3e0328a5e4aaeb510d5a10a912316e
xola-mthembu <xola.mthembu@gmail.com>
2024-09-30 08:57:53 -0700

    Implement server

---
+++ server.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
