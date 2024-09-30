23d1ebfed52fdf9ea26cc124bb1d67cc93e3005d
Sihle Mtshali <sihle.mtshali8@gmail.com>
2024-09-30 14:39:36 -0700

    Update server.js to use error handler and logger

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 #!/usr/bin/node
 import express from 'express';
 import routes from './routes/index';
+import errorHandler from './middleware/errorHandler';
+import { logger } from './utils/logger';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/', routes);
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  logger.info(`Server running on port ${port}`);
 });
+
+export default app; // Export for testing
