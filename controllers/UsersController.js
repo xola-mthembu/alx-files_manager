94a39be75ce2575aa1d43c3d3eaf93c7f1bb1d9b
xola-mthembu <xola.mthembu@gmail.com>
2024-10-02 11:49:36 -0700

    Implement UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,6 +2,7 @@
 import sha1 from 'sha1';
 import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -31,6 +32,27 @@ class UsersController {
       id: newUser.insertedId,
       email,
     });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = await redisClient.get(`auth_${token}`);
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const usersCollection = dbClient.client.db().collection('users');
+    const user = await usersCollection.findOne({ _id: ObjectId(userId) });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
