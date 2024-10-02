c7a1c1430b09855cb5f7da23603575bac900d209
xola-mthembu <xola.mthembu@gmail.com>
2024-10-02 10:37:27 -0700

    Task 2: First API

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,21 +1,20 @@
+#!/usr/bin/env node
+
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
-    const status = {
+    res.status(200).json({
       redis: redisClient.isAlive(),
       db: dbClient.isAlive(),
-    };
-    res.status(200).json(status);
+    });
   }
 
   static async getStats(req, res) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    res.status(200).json(stats);
+    const users = await dbClient.nbUsers();
+    const files = await dbClient.nbFiles();
+    res.status(200).json({ users, files });
   }
 }
 
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
+#!/usr/bin/env node
+
 import express from 'express';
-import routes from './routes/index';
+import router from './routes/index';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/', routes);
+app.use('/', router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-export default app;
