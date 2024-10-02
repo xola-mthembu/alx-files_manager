e478623d4877d554fda14e7485fc820cbfff2d84
xola-mthembu <xola.mthembu@gmail.com>
2024-10-02 10:42:15 -0700

    Task 3: Create a new user

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,7 @@
+#!/usr/bin/env node
+
 import sha1 from 'sha1';
-import { ObjectId } from 'mongodb';
 import dbClient from '../utils/db';
-import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -10,42 +10,26 @@ class UsersController {
     if (!email) {
       return res.status(400).json({ error: 'Missing email' });
     }
+
     if (!password) {
       return res.status(400).json({ error: 'Missing password' });
     }
 
-    const existingUser = await dbClient.db.collection('users').findOne({ email });
-    if (existingUser) {
+    const userExists = await dbClient.db
+      .collection('users')
+      .findOne({ email });
+
+    if (userExists) {
       return res.status(400).json({ error: 'Already exist' });
     }
 
     const hashedPassword = sha1(password);
-    const newUser = {
+    const result = await dbClient.db.collection('users').insertOne({
       email,
       password: hashedPassword,
-    };
+    });
 
-    const result = await dbClient.db.collection('users').insertOne(newUser);
     return res.status(201).json({ id: result.insertedId, email });
-  }
-
-  static async getMe(req, res) {
-    const token = req.header('X-Token');
-    if (!token) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const userId = await redisClient.get(`auth_${token}`);
-    if (!userId) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
-    if (!user) {
-      return res.status(401).json({ error: 'Unauthorized' });
-    }
-
-    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,3 @@
-#!/usr/bin/env node
+import UsersController from '../controllers/UsersController';
 
-import { Router } from 'express';
-import AppController from '../controllers/AppController';
-
-const router = Router();
-
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-
-export default router;
+router.post('/users', UsersController.postNew);
