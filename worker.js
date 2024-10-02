be11c2c0fc5b852fe1da0d3b1f65c5811bc9a10f
xola-mthembu <xola.mthembu@gmail.com>
2024-10-02 12:11:25 -0700

    Implement worker for thumbnail generation

--- worker.js
+++ worker.js
@@ -1,54 +1,43 @@
 #!/usr/bin/node
-import Bull from 'bull';
+import Queue from 'bull';
 import imageThumbnail from 'image-thumbnail';
-import fs from 'fs';
 import { ObjectId } from 'mongodb';
+import fs from 'fs';
 import dbClient from './utils/db';
 
-const fileQueue = new Bull('fileQueue');
-const userQueue = new Bull('userQueue');
+const fileQueue = new Queue('fileQueue');
 
-async function generateThumbnail(width, localPath) {
-  const thumbnail = await imageThumbnail(localPath, { width });
-  const thumbnailPath = `${localPath}_${width}`;
-  await fs.promises.writeFile(thumbnailPath, thumbnail);
-}
+const generateThumbnail = async (path, options) => {
+  try {
+    const thumbnail = await imageThumbnail(path, options);
+    const thumbnailPath = `${path}_${options.width}`;
+    await fs.promises.writeFile(thumbnailPath, thumbnail);
+  } catch (error) {
+    console.error('Error generating thumbnail:', error);
+  }
+};
 
 fileQueue.process(async (job) => {
   const { userId, fileId } = job.data;
+
   if (!fileId) {
     throw new Error('Missing fileId');
   }
+
   if (!userId) {
     throw new Error('Missing userId');
   }
 
-  const file = await dbClient.client.db().collection('files').findOne({
-    _id: ObjectId(fileId),
-    userId: ObjectId(userId),
-  });
+  const filesCollection = dbClient.client.db().collection('files');
+  const file = await filesCollection.findOne({ _id: ObjectId(fileId), userId: ObjectId(userId) });
 
   if (!file) {
     throw new Error('File not found');
   }
 
   const sizes = [500, 250, 100];
-  await Promise.all(sizes.map((size) => generateThumbnail(size, file.localPath)));
+  const thumbnailPromises = sizes.map((size) => generateThumbnail(file.localPath, { width: size }));
+  await Promise.all(thumbnailPromises);
 });
 
-userQueue.process(async (job) => {
-  const { userId } = job.data;
-  if (!userId) {
-    throw new Error('Missing userId');
-  }
-
-  const user = await dbClient.client.db().collection('users').findOne({
-    _id: ObjectId(userId),
-  });
-
-  if (!user) {
-    throw new Error('User not found');
-  }
-
-  console.log(`Welcome ${user.email}!`);
-});
+console.log('Worker is running');
